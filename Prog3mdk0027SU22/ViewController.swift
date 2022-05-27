a964a4c4469985347ac1611313ca15bb807525c8
mattwbdv <koenigmattd@gmail.com>
2022-05-26 20:59:00 -0700

    starting project

--- Prog3mdk0027SU22/ViewController.swift
+++ Prog3mdk0027SU22/ViewController.swift
@@ -14,6 +14,29 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var UA_Image: UIImageView!
+    
+    @IBOutlet weak var AU_Image: UIImageView!
+    
+    @IBOutlet weak var SEC_Image: UIImageView!
 
+    @IBAction func AU_Button(_ sender: Any) {
+        UA_Image.isHidden = true
+        SEC_Image.isHidden = true
+        AU_Image.isHidden = false
+    }
+    
+    @IBAction func SEC_Button(_ sender: Any) {
+        UA_Image.isHidden = true
+        SEC_Image.isHidden = false
+        AU_Image.isHidden = true
+    }
+    
+    @IBAction func UA_Button(_ sender: Any) {
+        UA_Image.isHidden = false
+        SEC_Image.isHidden = true
+        AU_Image.isHidden = true
+    }
+    
 }
 
